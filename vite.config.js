83b610ad3d8615d53f8ef20d4810777d340b1c62
convie-05 <convie-05@users.noreply.github.com>
2026-06-28 14:17:37 +0800

    fix: 修正 GitHub Pages base 路径为 /Serial-Port.github.io/

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
-    base: '/Serial-Port/',
+    base: '/Serial-Port.github.io/',
     plugins: [
       vue(),
 
